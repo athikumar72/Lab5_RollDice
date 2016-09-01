263e3ebc928c1970ae25bea3031eb11b7bd2a065
athikumar72 <athikumar72@gmail.com>
2016-09-01 08:42:26 -0400

    Today Rolldice change

---
+++ Roll_dice/Roll_dice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//using Lab5_RollDice;
+
+namespace Roll_dice
+{
+    class Program
+    {
+        string option;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Casino! Roll the dice ?(y/n");
+          string  option= Console.ReadLine();
+
+            while (option == "y") 
+            {
+                int roll1 = int.Parse(Console.ReadLine());
+                string i1 = Class1.Dice_Roll(roll1);
+
+                Console.WriteLine(i1);
+                Console.WriteLine("Roll again?(y/n)");
+               option= Console.ReadLine();
+            } 
+
+
+
+
+
+        }
+    }
+}
